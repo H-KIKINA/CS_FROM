d25b5d4800a4f311a7dfff4cc82d9a32393d0087
H-KIKINA <zeal24360216@fsgcl.jp>
2024-11-15 13:52:54 +0900

    different Button name update

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -12,27 +12,55 @@ namespace CS_Form
 {
     public partial class Form1 : Form
     {
+        Test_Label _testLabel;
+
         public Form1()
         {
             InitializeComponent();
-            
+
+            string [] strs = 
+            {  "あ",
+               "か" ,
+               "さ",
+               "た" ,
+               "な" ,
+               "は" ,
+               "ま" ,
+               "や" ,
+               "ら" ,
+               "わ",
+            };
+
             
             for (int i = 0; i <= 9; i++)
             {
-               
-                TestButton testButton =new TestButton(i,(i%5)*100, (i/5)*100, 100,100);
-
-                Controls.Add(testButton);
-               
                 
+                TestButton testButton = new TestButton(this, strs[i], (i % 5) * 100, (i / 5) * 100, 100, 100);
+                
+                Controls.Add(testButton);
+            }
 
+             _testLabel =
+                new Test_Label("ラベルです", 10, 300, 100, 500);
+            Controls.Add(_testLabel);
+            //Label label = new Label();
+            //label.Location = new Point(30, 400);
+            //label.Text = "ラベルです";
+
+            //Controls.Add(label);
 
 
                 //TestButton testButton2 = new TestButton(100,100,100,100);
 
                 //Controls.Add(testButton2);
-            }
-
+        }
+        /// <summary>
+        /// ラベルの文字を更新
+        /// </summary>
+        /// <param name="str"></param>
+        public void LabelTextUpdate(string str)
+        {
+            _testLabel.TextUpdate(str);
         }
     }
 }
--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -11,18 +11,19 @@ namespace CS_Form
 {
     internal class TestButton : Button
     {
+        Form1 _form1;
         /// <summary>
         ///コンストラクタ
         ///クラスを生成したときに呼び出される
         /// </summary>
         
-        public TestButton(int id ,int x,int y,int width,int height) 
+        public TestButton(Form1 form1,string str ,int x,int y,int width,int height) 
         {
+            _form1 = form1;
             //ClickイベントにOnClick関数をと登録
             //ボタンをクリックしたときに登録した関数を実行します
             Click += OnClick;
-
-            Text = id .ToString();
+            Text = str.ToString();
 
             Location = new Point(x,y);
             Size = new Size(width,height);
@@ -36,8 +37,11 @@ namespace CS_Form
 
     public void OnClick(object sender, EventArgs e)
     {
-            MessageBox.Show(Text);
+            _form1.LabelTextUpdate(Text);
     }
-}
+       
+
+
+    }
 
 }
